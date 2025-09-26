15156cac531e8b61ae489161c721fd8ec283e0ca
Tomás Romero Cejudo <Tomas.Romero1@alu.uclm.es>
2025-09-26 11:31:15 +0200

    Hecha la clase TipoBocadillo

---
+++ src/AppForSEII2526.API/Models/TipoBocadillo.cs
@@ -0,0 +1,31 @@
+﻿
+namespace AppForSEII2526.API.Models
+{
+    public class TipoBocadillo
+    {
+        public TipoBocadillo(int panid, string nombreTipo)
+        {
+            PanId = panid;
+            NombreTipo = nombreTipo;
+        }
+
+        [Key]
+        public int PanId { get; set; }
+        [Required]
+        public string NombreTipo { get; set; }
+
+        IList<TipoBocadillo> bocadillos = new List<TipoBocadillo>();
+
+        public override bool Equals(object? obj)
+        {
+            return obj is TipoBocadillo bocadillo &&
+                   PanId == bocadillo.PanId &&
+                   NombreTipo == bocadillo.NombreTipo;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(PanId, NombreTipo);
+        }
+    }
+}
