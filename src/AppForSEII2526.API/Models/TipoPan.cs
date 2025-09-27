f2eca8c099c771a641155acf7fe5395e6a792ecc
JESÚS LÓPEZ RONCERO <Jesus.Lopez22@alu.uclm.es>
2025-09-27 09:36:31 +0200

    Hago un commit con la clase TipoPan creada

---
+++ src/AppForSEII2526.API/Models/TipoPan.cs
@@ -0,0 +1,30 @@
+﻿
+namespace AppForSEII2526.API.Models
+{
+    public class TipoPan
+    {
+        public TipoPan(string nombre,int panId)
+        {
+            PanId = panId;
+            Nombre = nombre;
+        }
+        [Key]
+        public int PanId { get; set; }
+        [Required]
+        public string Nombre { get; set; }
+
+        IList<TipoPan> panes = new List<TipoPan>();
+
+        public override bool Equals(object? obj)
+        {
+            return obj is TipoPan pan &&
+                   PanId == pan.PanId &&
+                   Nombre == pan.Nombre;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(PanId, Nombre);
+        }
+    }
+}
