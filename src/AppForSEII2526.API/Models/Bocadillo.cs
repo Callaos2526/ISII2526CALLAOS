86f29798090df86fa24fd376926b91cd3660dbff
JESÚS LÓPEZ RONCERO <Jesus.Lopez22@alu.uclm.es>
2025-09-27 09:45:26 +0200

    Commit con la clase Bocadilllo creada

---
+++ src/AppForSEII2526.API/Models/Bocadillo.cs
@@ -0,0 +1,46 @@
+﻿
+namespace AppForSEII2526.API.Models
+{
+    public class Bocadillo
+    {
+        public Bocadillo(int id, string Nombre ,int pvp, string resenyaBocadillo,int stock,int tamaño)
+        {
+            Id = id;
+            nombre = Nombre;
+            PVP= pvp;
+            ResenyaBocadillo = resenyaBocadillo;
+            Stock = stock;
+            Tamaño = tamaño;
+        }
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string nombre { get; set; }
+
+        [Required]
+        public int PVP { get; set; }
+
+        [Required]
+        public string ResenyaBocadillo { get; set; }
+        [Required]
+        public int Stock { get; set; }
+        [Required]
+        public int Tamaño { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Bocadillo bocadillo &&
+                   Id == bocadillo.Id &&
+                   nombre == bocadillo.nombre &&
+                   PVP == bocadillo.PVP &&
+                   ResenyaBocadillo == bocadillo.ResenyaBocadillo &&
+                   Stock == bocadillo.Stock &&
+                   Tamaño == bocadillo.Tamaño;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id, nombre, PVP, ResenyaBocadillo, Stock, Tamaño);
+        }
+    }
+}
