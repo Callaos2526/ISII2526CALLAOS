fec0aba84dacd89ee1c74eb5c6def1557fcbfb2f
JESÚS LÓPEZ RONCERO <Jesus.Lopez22@alu.uclm.es>
2025-09-27 09:53:06 +0200

    Commit con la clase CompraBocadillo hecha

---
+++ src/AppForSEII2526.API/Models/CompraBocadillo.cs
@@ -0,0 +1,44 @@
+﻿
+namespace AppForSEII2526.API.Models
+{
+    public class CompraBocadillo
+    {
+        public CompraBocadillo(int bocadilloId,int cantidad,int compraId,string nombreBocadillo,int precio,TipoPan tipopan)
+        {
+            BocadilloId = bocadilloId;
+            Cantidad = cantidad;
+            CompraId = compraId;
+            NombreBocadillo = nombreBocadillo;
+            Precio = precio;
+            Tipopan = tipopan;
+        }
+        [Required]
+        public int BocadilloId { get; set; }
+        [Required]
+        public int Cantidad { get; set; }
+        [Key]
+        public int CompraId { get; set; }
+        [Required]
+        public string NombreBocadillo { get; set; }
+        [Required]
+        public int Precio { get; set; }
+        [Required]
+        public TipoPan Tipopan { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is CompraBocadillo bocadillo &&
+                   BocadilloId == bocadillo.BocadilloId &&
+                   Cantidad == bocadillo.Cantidad &&
+                   CompraId == bocadillo.CompraId &&
+                   NombreBocadillo == bocadillo.NombreBocadillo &&
+                   Precio == bocadillo.Precio &&
+                   EqualityComparer<TipoPan>.Default.Equals(Tipopan, bocadillo.Tipopan);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(BocadilloId, Cantidad, CompraId, NombreBocadillo, Precio, Tipopan);
+        }
+    }
+}
