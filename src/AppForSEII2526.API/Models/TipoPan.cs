589768f47bc1ff6e977c4d3302093ef22e00d867
MARTA TOSCA PEREZ <Marta.Tosca@alu.uclm.es>
2025-10-02 21:56:14 +0200

    Añado lista de Bocadillo a clase TipoPan de CU-PedirBocadillo

--- src/AppForSEII2526.API/Models/TipoPan.cs
+++ src/AppForSEII2526.API/Models/TipoPan.cs
@@ -1,7 +1,11 @@
-﻿namespace AppForSEII2526.API.Models
+﻿
+namespace AppForSEII2526.API.Models
 {
     public class TipoPan
     {
+        //constructor vacio
+        public TipoPan() { }
+        //constructor con parametros
         public TipoPan(int panid, string nombre)
         {
             PanId = panid;
@@ -11,18 +15,24 @@
         [Key]
         public int PanId { get; set; }
         [Required]
-        public string Nombre { get; set; }
+        public required string Nombre { get; set; } //obliga a que no sea null aunque uses constructor vacio
+        //Vector de relacion 1:N con Bocadillo
+        [Required]
+        public IList<Bocadillo> Bocadillos { get; set; }=new List<Bocadillo>();
 
         public override bool Equals(object? obj)
         {
             return obj is TipoPan pan &&
                    PanId == pan.PanId &&
-                   Nombre == pan.Nombre;
+                   Nombre == pan.Nombre &&
+                   EqualityComparer<IList<Bocadillo>>.Default.Equals(Bocadillos, pan.Bocadillos);
         }
 
         public override int GetHashCode()
         {
-            return HashCode.Combine(PanId, Nombre);
+            return HashCode.Combine(PanId, Nombre, Bocadillos);
         }
+
+      
     }
 }
