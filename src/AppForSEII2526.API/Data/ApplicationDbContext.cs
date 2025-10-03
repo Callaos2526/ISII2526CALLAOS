e32503d3bf76e004de26f4a55d3e248f1986263a
MARTA TOSCA PEREZ <Marta.Tosca@alu.uclm.es>
2025-10-03 18:50:51 +0200

    Edito las clases de metodo Pago y las añado a DBContext

--- src/AppForSEII2526.API/Data/ApplicationDbContext.cs
+++ src/AppForSEII2526.API/Data/ApplicationDbContext.cs
@@ -5,4 +5,19 @@ using AppForSEII2526.API.Models;
 namespace AppForSEII2526.API.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options) {
+    public DbSet<Bocadillo> Bocadillos { get; set; }
+    public DbSet<Compra> Compras { get; set; }
+    public DbSet<CompraBocadillo> ComprasBocadillos { get; set; }
+    public DbSet<TipoPan> TiposPan { get; set; }
+    public DbSet<MetodoPago> MetodosPago { get; set; }
+    public DbSet<GooglePay> GooglePay { get; set; }
+    public DbSet<Tarjeta> Tarjeta { get; set; }
+    public DbSet<Paypal> Paypal { get; set; }
+    protected override void OnModelCreating(ModelBuilder builder) {
+        base.OnModelCreating(builder);
+        //Composite key for CompraBocadillo
+        builder.Entity<CompraBocadillo>()
+            .HasKey(cb => new { cb.CompraId, cb.BocadilloId });
+        
+    }
 }
---
+++ src/AppForSEII2526.API/Models/Tarjeta.cs
@@ -0,0 +1,12 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    public class Tarjeta : MetodoPago
+    {
+        public Tarjeta()
+        {
+            metodoName = "Tarjeta";
+        }
+
+
+    }
+}
