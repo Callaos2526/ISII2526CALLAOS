282ba29be5b42ad43c683aed3c113e44e5ac411f
JESÚS LÓPEZ RONCERO <Jesus.Lopez22@alu.uclm.es>
2025-10-15 17:19:41 +0200

    creo mi controlador

---
+++ src/AppForSEII2526.API/Controller/Controlador.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppForSEII2526.API.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Controlador : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger<Controlador> _logger;
+
+        public Controlador(ApplicationDbContext context, ILogger<Controlador> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+
+    }
+}
