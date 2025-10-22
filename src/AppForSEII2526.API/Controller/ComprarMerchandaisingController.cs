420ceecad9078c364604358660a3ea13f860b659
MIGUEL REQUENA LÓPEZ <Miguel.Requena@alu.uclm.es>
2025-10-22 17:26:58 +0200

    Cambio los nombre del DTO y controller para ponerlos como en el enunciado

--- src/AppForSEII2526.API/Controller/ComprarMerchandaisingController.cs
+++ src/AppForSEII2526.API/Controller/ComprarMerchandaisingController.cs
@@ -6,28 +6,28 @@ namespace AppForSEII2526.API.Controller
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class CompraProductoController : ControllerBase
+    public class ComprarMerchandaisingController : ControllerBase
     {
         private readonly ApplicationDbContext _context; 
-        private readonly ILogger<CompraProductoController> _logger;
-        public CompraProductoController(ApplicationDbContext context, ILogger<CompraProductoController> logger)
+        private readonly ILogger<ComprarMerchandaisingController> _logger;
+        public ComprarMerchandaisingController(ApplicationDbContext context, ILogger<ComprarMerchandaisingController> logger)
         {
             _context = context;
             _logger = logger;
         }
         [HttpGet]
         [Route("[action]")]
-        [ProducesResponseType(typeof(IList<MerchDTO>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(IList<ComprarMerchandaisingDTO>), (int)HttpStatusCode.OK)]
         [ProducesResponseType((int)HttpStatusCode.NotFound)]
         public async Task<ActionResult> GetMerchParaCompra(string? filtroTipo, int? filtroPrecio)
         {
-            IList<MerchDTO> productos = await _context.Producto
+            IList<ComprarMerchandaisingDTO> productos = await _context.Producto
                 .Include(tp => tp.TipoProducto)
                 .Include(tp => tp.producto_Compras).ThenInclude(c => c.compra)
                 .Where(producto => (filtroTipo == null || producto.TipoProducto.Nombre.Equals(filtroTipo)) &&
                                 (filtroPrecio == null || producto.PVP >= filtroPrecio))
                 .OrderBy(producto => producto.Nombre)
-                .Select(p => new MerchDTO(p.Productoid, p.Nombre, p.PVP, p.Stock, p.TipoProducto.ToString()))
+                .Select(p => new ComprarMerchandaisingDTO(p.Productoid, p.Nombre, p.PVP, p.Stock, p.TipoProducto.ToString()))
                 .ToListAsync();
             if (productos == null || !productos.Any())
             {
