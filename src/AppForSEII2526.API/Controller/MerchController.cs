1981c7aafe1ba6316ab5e7ad87f4a999ab84ca24
MIGUEL REQUENA LÓPEZ <Miguel.Requena@alu.uclm.es>
2025-10-22 17:16:46 +0200

    He acabado el controlador y el dto de merch (Paso 2)

---
+++ src/AppForSEII2526.API/Controller/MerchController.cs
@@ -0,0 +1,39 @@
+﻿using AppForSEII2526.API.DTOs.ComprarMerch;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppForSEII2526.API.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompraProductoController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context; 
+        private readonly ILogger<CompraProductoController> _logger;
+        public CompraProductoController(ApplicationDbContext context, ILogger<CompraProductoController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(IList<MerchDTO>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public async Task<ActionResult> GetMerchParaCompra(string? filtroTipo, int? filtroPrecio)
+        {
+            IList<MerchDTO> productos = await _context.Producto
+                .Include(tp => tp.TipoProducto)
+                .Include(tp => tp.producto_Compras).ThenInclude(c => c.compra)
+                .Where(producto => (filtroTipo == null || producto.TipoProducto.Nombre.Equals(filtroTipo)) &&
+                                (filtroPrecio == null || producto.PVP >= filtroPrecio))
+                .OrderBy(producto => producto.Nombre)
+                .Select(p => new MerchDTO(p.Productoid, p.Nombre, p.PVP, p.Stock, p.TipoProducto.ToString()))
+                .ToListAsync();
+            if (productos == null || !productos.Any())
+            {
+                return NotFound("No hay productos que cumplan los requisitos");
+            }
+            return Ok(productos);
+        }
+    }
+}
---
+++ src/AppForSEII2526.API/DTOs/ComprarMerch/MerchDTO.cs
@@ -0,0 +1,19 @@
+﻿namespace AppForSEII2526.API.DTOs.ComprarMerch
+{
+    public class MerchDTO
+    {
+        public int ProductoId { get; set; }
+        public string Nombre { get; set; }
+        public decimal PVP { get; set; }
+        public int Stock { get; set; }
+        public string Tipo { get; set; }
+        public MerchDTO(int productoId, string nombre, decimal pvp, int stock, string tipo)
+        {
+            ProductoId = productoId;
+            Nombre = nombre;
+            PVP = pvp;
+            Stock = stock;
+            Tipo = tipo;
+        }
+    }
+}
