3b8343a9967e9bf1b71e0807b71540eefe3ea576
MARTA TOSCA PÉREZ <Marta.Tosca@alu.uclm.es>
2025-11-11 16:05:12 +0100

    Cambios en el post (modificación de código para solucionar errores)

--- src/AppForSEII2526.API/Data/ApplicationDbContext.cs
+++ src/AppForSEII2526.API/Data/ApplicationDbContext.cs
@@ -27,44 +27,28 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
     public DbSet<ApplicationUser> ApplicationUsers { get; set; }
     public DbSet<MetodoPago> MetodoPago { get; set; }
 
-    public DbSet<ApplicationUser> ApplicationUser { get; set; }
-
-
-
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
 
         builder.Entity<MetodoPago>()
-        .HasDiscriminator<string>("Discriminator")
-        .HasValue<MetodoPago>("MetodoPago")
-        .HasValue<Tarjeta>("Tarjeta")
-        .HasValue<Paypal>("Paypal")
-        .HasValue<GooglePay>("GooglePay");
+            .HasDiscriminator<string>("Discriminator")
+            .HasValue<MetodoPago>("MetodoPago")
+            .HasValue<Tarjeta>("Tarjeta")
+            .HasValue<Paypal>("Paypal")
+            .HasValue<GooglePay>("GooglePay");
 
         builder.Entity<BonosComprados>()
              .HasAlternateKey(bc => new { bc.CompraId, bc.BonoId });
+
         builder.Entity<ResenyaBocadillo>()
             .HasKey(rb => new { rb.BocadilloId, rb.ResenyaId });
-        // builder.Entity<CompraBocadillo>()
-        //  .HasKey(cb => new { cb.CompraId, cb.BocadilloId });
+
         builder.Entity<CompraBocadillo>()
-         .HasIndex(cb => new { cb.CompraId, cb.BocadilloId })
-      .IsUnique();
+            .HasIndex(cb => new { cb.CompraId, cb.BocadilloId })
+            .IsUnique();
+
         builder.Entity<Producto_Compra>()
-           .HasKey(pc => new { pc.Compraid, pc.Productoid});
-        builder.Entity<MetodoPago>()
-        .HasDiscriminator<string>("Discriminator")
-        .HasValue<MetodoPago>("MetodoPago")
-        .HasValue<Tarjeta>("Tarjeta")
-        .HasValue<Paypal>("Paypal")
-        .HasValue<GooglePay>("GooglePay");
+           .HasKey(pc => new { pc.Compraid, pc.Productoid });
     }
-
-
 }
-    
-
-    
-    
-
