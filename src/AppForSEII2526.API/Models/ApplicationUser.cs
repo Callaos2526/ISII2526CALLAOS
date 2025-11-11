2d1245d843190af95a2b07bb618a9fcd890569ce
MARTA TOSCA PÉREZ <Marta.Tosca@alu.uclm.es>
2025-11-11 16:04:31 +0100

    Cambios en la clase ApplicationUser

--- src/AppForSEII2526.API/Models/ApplicationUser.cs
+++ src/AppForSEII2526.API/Models/ApplicationUser.cs
@@ -8,14 +8,14 @@ public class ApplicationUser : IdentityUser {
     {
     }
 
-    public ApplicationUser(int id, string nombreCliente, string apellidoCliente1, string apellidoCliente2)
+    public ApplicationUser( string nombreCliente, string apellidoCliente1, string apellidoCliente2)
     {
-        ID = id;
+      
         NombreCliente = nombreCliente;
         ApellidoCliente1 = apellidoCliente1;
         ApellidoCliente2 = apellidoCliente2;
     }
-    public int ID { get; set; }
+    // public int ID { get; set; } // Miguel que cambie lo del id por compraid en su clase
     public string NombreCliente { get; set; }
     public string ApellidoCliente1 { get; set; }
     public string ApellidoCliente2 { get; set; }
