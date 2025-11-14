ab4b54982a43e00f9d15ac9bbdedb475223ba47d
MARTA TOSCA PÉREZ <Marta.Tosca@alu.uclm.es>
2025-11-14 18:45:12 +0100

    Cambios realizados

--- src/AppForSEII2526.API/Models/ApplicationUser.cs
+++ src/AppForSEII2526.API/Models/ApplicationUser.cs
@@ -8,9 +8,9 @@ public class ApplicationUser : IdentityUser {
     {
     }
 
-    public ApplicationUser( string nombreCliente, string apellidoCliente1, string apellidoCliente2)
+    public ApplicationUser(string nombreCliente, string apellidoCliente1, string apellidoCliente2)
     {
-      
+        
         NombreCliente = nombreCliente;
         ApellidoCliente1 = apellidoCliente1;
         ApellidoCliente2 = apellidoCliente2;
