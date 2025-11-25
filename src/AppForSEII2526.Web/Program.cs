4f4d39328109d7a0db99a6fd6406d0d436cb3d41
MIGUEL REQUENA LÓPEZ <Miguel.Requena@alu.uclm.es>
2025-11-25 17:05:34 +0100

    Añado el MerchStateContainer como servicio en Program.cs

--- src/AppForSEII2526.Web/Program.cs
+++ src/AppForSEII2526.Web/Program.cs
@@ -1,3 +1,4 @@
+using AppForSEII2526.Web;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -5,7 +6,11 @@ using AppForSEII2526.Web.Components;
 using AppForSEII2526.Web.Components.Account;
 using AppForSEII2526.Web.Data;
 
+
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddScoped<CompraMerchStateContainer>();
+
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
