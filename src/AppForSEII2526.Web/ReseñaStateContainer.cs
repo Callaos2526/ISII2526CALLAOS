a4ddfcbd2d4ef7a8698a0652de6870abb31426e1
JESÚS LÓPEZ RONCERO <Jesus.Lopez22@alu.uclm.es>
2025-11-27 16:56:46 +0100

    hago el StateContainer

---
+++ src/AppForSEII2526.Web/ReseñaStateContainer.cs
@@ -0,0 +1,65 @@
+﻿using AppForSEII2526.API.DTOs.ResenyaDTOs;
+
+namespace AppForSEII2526.Web
+{
+    public class ResenyaStateContainer
+    {
+        // La reseña que se está creando
+        public ResenyaForCreateDTO Resenya { get; private set; } = new ResenyaForCreateDTO()
+        {
+            ResenyaBocadillo = new List<ResenyaItemDTO>()
+        };
+
+        public event Action? OnChange;
+        private void NotifyStateChanged() => OnChange?.Invoke();
+
+        //   AÑADIR BOCADILLOS A LA RESEÑA
+        public void AddBocadillo(int bocadilloId, int puntuacion)
+        {
+            if (puntuacion < 1 || puntuacion > 10)
+                return;
+
+            var existing = Resenya.ResenyaBocadillo
+                .FirstOrDefault(i => i.BocadilloId == bocadilloId);
+
+            if (existing != null)
+            {
+                // Si ya existe ese bocadillo, solo se actualiza la puntuación
+                existing.Puntuacion = puntuacion;
+            }
+            else
+            {
+                // Si no existe, se añade uno nuevo
+                Resenya.ResenyaBocadillo.Add(new ResenyaItemDTO(
+                    bocadilloId,
+                    puntuacion
+                ));
+            }
+
+            NotifyStateChanged();
+        }
+        //   ELIMINAR BOCADILLO
+        public void RemoveBocadillo(ResenyaItemDTO item)
+        {
+            Resenya.ResenyaBocadillo.Remove(item);
+            NotifyStateChanged();
+        }
+
+        public void ClearResenya()
+        {
+            Resenya.ResenyaBocadillo.Clear();
+            NotifyStateChanged();
+        }
+
+        public void ResenyaProcessed()
+        {
+            Resenya = new ResenyaForCreateDTO()
+            {
+                ResenyaBocadillo = new List<ResenyaItemDTO>()
+            };
+
+            NotifyStateChanged();
+        }
+    }
+}
+
