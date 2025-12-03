a6c9fb4033eb06a24a7f675d4bde674962139ec7
T0M111 <tomas.romero1@alu.uclm.es>
2025-12-03 16:32:51 +0100

    Hago el StateContainer de bonos

---
+++ src/AppForSEII2526.Web/BonoStateContainer.cs
@@ -0,0 +1,70 @@
+﻿
+using AppForSEII2526.API.DTOs.CompraBonoDTOs;
+
+namespace AppForSEII2526.Web
+{
+    public class BonoStateContainer
+    {
+        public CrearCompraDTO Compra { get; private set; } = new CrearCompraDTO()
+        {
+            BonoItem = new List<BonoItemForCreateDTO>()
+        };
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                return Compra.BonoItem.Sum(i => Convert.ToDecimal(i.Precio) * i.Cantidad);
+            }
+        }
+
+        public event Action? OnChange;
+        private void NotifyStateChanged() => OnChange?.Invoke();
+
+        public void AddProduct(BonoItemForCreateDTO item)
+        {
+            if (item == null) return;
+
+            // Si ya existe, incrementa cantidad; si no, lo añade
+            var existing = Compra.BonoItem.FirstOrDefault(i => i.BonoId == item.BonoId);
+            if (existing != null)
+            {
+                existing.Cantidad += item.Cantidad;
+            }
+            else
+            {
+                Compra.BonoItem.Add(new BonoItemForCreateDTO(
+                    item.BonoId,
+                    item.Cantidad,
+                    item.Nombre,
+                    item.Precio,
+                    item.NumeroDeBocadillos,
+                    item.Tipo
+                ));
+            }
+
+            NotifyStateChanged();
+        }
+
+        // Eliminar un producto concreto del carrito
+        public void RemoveItem(BonoItemForCreateDTO item)
+        {
+            Compra.BonoItem.Remove(item);
+        }
+
+        // Vaciar el carrito
+        public void ClearCart()
+        {
+            Compra.BonoItem.Clear();
+        }
+
+        // Reset del estado cuando la compra se ha procesado
+        public void CompraProcessed()
+        {
+            Compra = new CrearCompraDTO()
+            {
+                BonoItem = new List<BonoItemForCreateDTO>()
+            };
+        }
+    }
+}
