b7856d911fe9c48f1653588d8dfff3a476e6a455
MIGUEL REQUENA LÓPEZ <Miguel.Requena@alu.uclm.es>
2025-12-10 17:11:06 +0100

    Creo la organización de carpetas para las pruebas

---
+++ test/AppForSEII2526.UIT/ComprarMerch/SelectComprarMerch_PO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Support.UI;
+
+namespace AppForSEII2526.UIT.ComprarMerch
+{
+    internal class SelectComprarMerch_PO 
+    {
+    }
+}
