bf58e006d0a725be4cd5c0f9fcc60528806c06fc
MIGUEL REQUENA LÓPEZ <Miguel.Requena@alu.uclm.es>
2025-12-11 11:45:15 +0100

    Corrijo errores y creo las clases necesarias para desarrollar las pruebas

--- test/AppForSEII2526.UIT/ComprarMerch/SelectComprarMerch_UIT.cs
+++ test/AppForSEII2526.UIT/ComprarMerch/SelectComprarMerch_UIT.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using AppForSEII2526.UIT.Shared;
 namespace AppForSEII2526.UIT.ComprarMerch
 {
-    internal class SelectComprarMerch_UIT
+    internal class SelectComprarMerch_UIT : UC_UIT
     {
+        public SelectComprarMerch_UIT(ITestOutputHelper output) : base(output)
+        {
+        }
     }
 }
