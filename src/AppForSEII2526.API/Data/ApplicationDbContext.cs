d6bbff259b682b18ab65b7d74ccee2aea95f694a
jesusscyro <jesus.lopez22@alu.uclm.es>
2025-12-12 16:41:21 +0100

    136 uit get (#143)
    * hago mis clases
    
    * cambios
    
    * cambios
    
    * cambios
    
    * finalizo

--- src/AppForSEII2526.API/Data/ApplicationDbContext.cs
+++ src/AppForSEII2526.API/Data/ApplicationDbContext.cs
@@ -22,6 +22,7 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
     public DbSet<ResenyaBocadillo> ResenyasBocadillo { get; set; }
     public DbSet<Resenya> Resenyas { get; set; }
     public DbSet<Compra> Compras { get; set; }
+    
 
     public DbSet<CompraBocadillo> ComprasBocadillos { get; set; }
     public DbSet<ApplicationUser> ApplicationUsers { get; set; }
